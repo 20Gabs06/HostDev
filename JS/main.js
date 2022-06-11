413dba5be0fb2df14d2a287123b66d7334d06516
/gabs <gabrielbernardo2006sb@gmail.com>
2022-06-11 09:45:15 -0300

    Save
    ajustes

--- JS/main.js
+++ JS/main.js
@@ -1,6 +1,17 @@
   function openMenu(){
-    
+    const addBody = document.querySelector("body")
+    const addMenu = document.querySelector("#menu")
+    addBody.classList.add('MenuOn')
+    addMenu.classList.remove('Close')
+    addMenu.classList.add('Open')
 
   }
 
+  function CloseMenu(){
+    const addBody = document.querySelector("body")
+    const addMenu = document.querySelector("#menu")
+    addBody.classList.remove('MenuOn')
+    addMenu.classList.remove('Open')
+    addMenu.classList.add('Close')
+  }
 
